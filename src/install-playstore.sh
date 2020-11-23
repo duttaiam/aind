87032825985d9769c05122b2323eba399dc77827
Niek van der Maas <mail@niekvandermaas.nl>
2020-11-23 12:27:43 +0100

    Try GLES 1.1

--- src/install-playstore.sh
+++ src/install-playstore.sh
@@ -111,7 +111,9 @@ sed -i "/<unavailable-feature name=\"android.hardware.bluetooth\" \/>/d" "$OVERL
 cp "$WORKDIR/squashfs-root/system/build.prop" "$OVERLAYDIR/system/build.prop"
 cp "$WORKDIR/squashfs-root/default.prop" "$OVERLAYDIR/default.prop"
 
-# enable opengles, 131072 = 2 in HEX
-echo "ro.opengles.version=131072" >> "$OVERLAYDIR/system/build.prop"
+# Set specific GLES version
+#echo "ro.opengles.version=196608" >> "$OVERLAYDIR/system/build.prop" # GLES 3.0
+#echo "ro.opengles.version=131072" >> "$OVERLAYDIR/system/build.prop" # GLES 2.0
+echo "ro.opengles.version=65536" >> "$OVERLAYDIR/system/build.prop" # GLES 1.1
 
 rm -rf $WORKDIR
