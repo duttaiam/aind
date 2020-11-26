1177e88123800d52826b61bbe9e154d2b381a78f
Niek van der Maas <mail@niekvandermaas.nl>
2020-11-26 10:57:36 +0100

    Allow --device

--- src/anbox-container-manager-pre.sh
+++ src/anbox-container-manager-pre.sh
@@ -6,20 +6,24 @@ if [ $(id -u) != 0 ]; then
 fi
 
 # ashmem
-/sbin/modprobe ashmem_linux
-if [ ! -e /dev/ashmem ]; then
-	mknod /dev/ashmem c 10 55
+if [ -f "/lib/modules/$(uname -r)/kernel/drivers/staging/android/ashmem_linux.ko" ]; then
+	/sbin/modprobe ashmem_linux
+	if [ ! -e /dev/ashmem ]; then
+		mknod /dev/ashmem c 10 55
+	fi
 fi
 
 # binder (newer kernel uses /dev/binderfs directory; older kernel uses /dev/binder file)
-/sbin/modprobe binder_linux
-if grep binder /proc/filesystems; then
-	if [ ! -e /dev/binderfs/binder-control ]; then
-		mkdir -p /dev/binderfs
-		mount -t binder none /dev/binderfs
-	fi
-else
-	if [ ! -e /dev/binder ]; then
-		mknod /dev/binder c 511 0
+if [ -f "/lib/modules/$(uname -r)/kernel/drivers/android/binder_linux.ko" ]; then
+	/sbin/modprobe binder_linux
+	if grep binder /proc/filesystems; then
+		if [ ! -e /dev/binderfs/binder-control ]; then
+			mkdir -p /dev/binderfs
+			mount -t binder none /dev/binderfs
+		fi
+	else
+		if [ ! -e /dev/binder ]; then
+			mknod /dev/binder c 511 0
+		fi
 	fi
 fi
