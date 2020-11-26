d8aced908d00406bb53534f19368c7d088b8d1dc
Niek van der Maas <mail@niekvandermaas.nl>
2020-11-26 21:25:05 +0100

    Support newer kernels

--- src/anbox-container-manager-pre.sh
+++ src/anbox-container-manager-pre.sh
@@ -6,24 +6,20 @@ if [ $(id -u) != 0 ]; then
 fi
 
 # ashmem
-if [ -f "/lib/modules/$(uname -r)/kernel/drivers/staging/android/ashmem_linux.ko" ]; then
-	/sbin/modprobe ashmem_linux
-	if [ ! -e /dev/ashmem ]; then
-		mknod /dev/ashmem c 10 55
-	fi
+/sbin/modprobe ashmem_linux
+if [ ! -e /dev/ashmem ]; then
+	mknod /dev/ashmem c 10 55
 fi
 
 # binder (newer kernel uses /dev/binderfs directory; older kernel uses /dev/binder file)
-if [ -f "/lib/modules/$(uname -r)/kernel/drivers/android/binder_linux.ko" ]; then
-	/sbin/modprobe binder_linux
-	if grep binder /proc/filesystems; then
-		if [ ! -e /dev/binderfs/binder-control ]; then
-			mkdir -p /dev/binderfs
-			mount -t binder none /dev/binderfs
-		fi
-	else
-		if [ ! -e /dev/binder ]; then
-			mknod /dev/binder c 511 0
-		fi
+/sbin/modprobe binder_linux
+if grep binder /proc/filesystems; then
+	if [ ! -e /dev/binderfs/binder-control ]; then
+		mkdir -p /dev/binderfs
+		mount -t binder none /dev/binderfs
+	fi
+else
+	if [ ! -e /dev/binder ]; then
+		mknod /dev/binder c 511 0
 	fi
 fi
